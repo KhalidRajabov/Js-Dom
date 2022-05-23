c3de6f6e58ce79e09933c39a60d614c0b404a344
dbBurzum <khalidsr@code.edu.az>
2022-05-24 03:49:34 +0400

    done

--- app.js
+++ app.js
@@ -5,17 +5,29 @@ btn.onclick = function () {
   let items_1 = list1.getElementsByTagName("li");
   let items_2 = list2.getElementsByTagName("li");
   let new_list = document.getElementById("new");
-
-  console.log(list1);
+  new_list.innerHTML=""
+  let newarr=[];
+  let listarray=[];
   for (let i = 0; i < items_1.length; i++) {
-    for (let j = 0; j < items_2.length; j++) {
-      if (items_1 != items_2) {
-        console.log(items_1.text, items_2);
-        let new_li = document.createElement("li");
-        new_list.append(new_li);
-        new_li.innerText = "";
-        console.log("works");
-      }
-    }
+    let text1=items_1[i].childNodes[0].nodeValue;  
+    newarr.push(text1);
   }
-};
+  for (let i = 0; i < items_2.length; i++) {
+    let text2=items_2[i].childNodes[0].nodeValue;  
+    newarr.push(text2);
+  }
+  
+  listarray= newarr.filter((item,index) => newarr.indexOf(item) ===index);
+  
+  for (let i = 0; i < listarray.length; i++) {
+    let new_li = document.createElement("li");
+    new_list.append(new_li);
+    new_li.innerText = listarray[i];
+  }
+  
+  console.log(listarray);
+
+}
+
+
+

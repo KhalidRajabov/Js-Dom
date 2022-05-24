1c34df7fb1722cb9354047f875bff932269c71e2
dbBurzum <khalidsr@code.edu.az>
2022-05-24 17:22:28 +0400

    99% done

--- assets/check.js
+++ assets/check.js
@@ -2,6 +2,7 @@ let new_list = document.getElementById("new");
 let btnClick = document.getElementById("btnClick");
 let btnRemove = document.getElementById("btnRemove");
 let input = document.getElementById("input");
+let btnRemoveSome = document.getElementById("btnRemoveSome")
 
 input.addEventListener("keypress", function (event) {
   if (event.key === "Enter") {
@@ -19,18 +20,29 @@ btnClick.onclick = function () {
   if (text != "") {
     let li = document.createElement("li");
     let icon = document.createElement("i")
-    icon.classList="fa-solid fa-delete-left"
+     icon.classList="fa-solid fa-delete-left"
+    let form = document.createElement("form")
+    form.innerHTML= `<input id="check" class="my-box" type="checkbox">`
     li.classList = "list-group-item list sorted";
     new_list.append(li);
     li.innerHTML = text;
-    li.append(icon)
+    form.append(icon)
+    li.append(form)
     icon.onclick=function(){
       li.remove();
+    }
+
+    let check = document.getElementById("check")
+    btnRemoveSome.onclick=function(){
+      if(check.checked){
+        parentElement.remove();
+      }
     }
   }
 }
 btnRemove.onclick = function () {
   new_list.innerHTML = "";
 };
+
 
 

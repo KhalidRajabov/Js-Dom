cb3c2572c8efbbddbcd008c5ec177925c7c44106
dbBurzum <khalidsr@code.edu.az>
2022-05-25 17:17:34 +0400

    updated

--- assets/calc.js
+++ assets/calc.js
@@ -11,34 +11,63 @@ let toResult = document.getElementById("area");
 plus.onclick = function () {
     let num1 = parseInt(first.value) 
     let num2 = parseInt(second.value) 
-    if (num1!="" && num2!="") {
+    
+    if (num1>=0 && num2>=0) {
         
         let answer=num1+num2;
         toResult.innerText=answer
         toResult.style.color = "blue";
     }
+    else{
+        toResult.innerText="Do net leave inputs empty"
+        toResult.style.color = "blue";
+    }
+    first.value=""
+    second.value=""
 };
 minus.onclick = function () {
     let num1 = parseInt(first.value) 
     let num2 = parseInt(second.value)
-    if (num1!="" && num2!="") {
+    if (num1>=0 && num2>=0) {
         
         let answer=num1-num2;
         toResult.innerText=answer
         toResult.style.color = "red";
+    }
+    else{
+        toResult.innerText="Do net leave inputs empty"
+        toResult.style.color = "red";
     } 
+    first.value=""
+    second.value=""
 };
 mult.onclick = function () {
     let num1 = parseInt(first.value) 
     let num2 = parseInt(second.value) 
-    let answer=num1*num2; 
-    toResult.innerText=answer
-    toResult.style.color = "green";
+    if (num1>=0 && num2>=0) {
+        let answer=num1*num2; 
+        toResult.innerText=answer
+        toResult.style.color = "green";
+    }
+    else{
+        toResult.innerText="Do net leave inputs empty"
+        toResult.style.color = "green";
+    }
+    first.value=""
+    second.value=""
 };
 divide.onclick = function () {
     let num1 = parseInt(first.value) 
     let num2 = parseInt(second.value) 
-    let answer=num1/num2;
-    toResult.innerText=answer
-    toResult.style.color = "orange";
+    if (num1>=0 && num2>=0) {
+        let answer=num1/num2;
+        toResult.innerText=answer
+        toResult.style.color = "orange";
+    }
+    else{
+        toResult.innerText="Do net leave inputs empty"
+        toResult.style.color = "orange";
+    }
+    first.value=""
+    second.value=""
 };
